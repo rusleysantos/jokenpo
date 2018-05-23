b5c8a50170e7da8ab6e9b8ca018905e0770ff490
rusleysantos <rusley_silva@hotmail.com>
2018-05-23 13:12:45 -0300

    Commit final

--- js/meujs.js
+++ js/meujs.js
@@ -1,5 +1,93 @@
-min = Math.ceil(1);
-max = Math.floor(4);
-var Maquina = Math.floor(Math.random() * (max - min)) + min;
+var Jogador;
+var MaquinaJogada;
+var Maquina;
+var PlacarJogador =0;
+var PlacarMaquina =0;
 
-alert('teste ' + Maquina);
+function Logica(Element) {
+    Jogador = Element.getAttribute("value");
+    min = Math.ceil(1);
+    max = Math.floor(4);
+    Maquina = Math.floor(Math.random() * (max - min)) + min;
+    
+    if (Maquina == 1) 
+    {
+        MaquinaJogada = "pedra";
+    } 
+    else if (Maquina == 2) 
+    {
+        MaquinaJogada = "papel";
+    } 
+    else if (Maquina == 3) 
+    {
+        MaquinaJogada = "tesoura";
+    }
+    //empate
+    if(Jogador==MaquinaJogada)
+    {
+       
+        alert('Você jogou '+Jogador+' e o computador '+MaquinaJogada);
+        alert('Resutado: Empate');
+        alert('O seu placar é '+PlacarJogador+' e o placar do computador '+PlacarMaquina+'.');
+    }
+     //Ganhou
+     else if(Jogador=='pedra' && MaquinaJogada=='tesoura')
+     {
+        alert('Você jogou '+Jogador+' e o computador '+MaquinaJogada+'.');
+        alert("Você ganhou!!!");
+        PlacarJogador++;
+        
+        alert('O seu placar é '+PlacarJogador+' e o placar do computador '+PlacarMaquina+'.');
+    }
+    
+    else if(Jogador=='papel' && MaquinaJogada=='pedra')
+     {
+        alert('Você jogou '+Jogador+' e o computador '+MaquinaJogada+'.');
+        alert("Você ganhou!!!");
+        PlacarJogador++;
+        
+        alert('O seu placar é '+PlacarJogador+' e o placar do computador '+PlacarMaquina+'.');
+    }
+    
+    else if(Jogador=='tesoura' && MaquinaJogada=='papel')
+     {
+        alert('Você jogou '+Jogador+' e o computador '+MaquinaJogada+'.');
+        alert("Você ganhou!!!");
+        PlacarJogador++;
+        
+        alert('O seu placar é '+PlacarJogador+' e o placar do computador '+PlacarMaquina+'.');
+    }
+    
+    //Perdeu
+    else if(MaquinaJogada=="pedra" && Jogador=="tesoura")
+    {
+        alert('Você jogou '+Jogador+' e o computador '+MaquinaJogada+'.');
+        alert("Você perdeu!!!");
+        PlacarMaquina++;
+        
+        alert('O seu placar é '+PlacarJogador+' e o placar do computador '+PlacarMaquina+'.');
+    }
+    
+     else if(MaquinaJogada=="papel" && Jogador=="pedra")
+    {
+        alert('Você jogou '+Jogador+' e o computador '+MaquinaJogada+'.');
+        alert("Você perdeu!!!");
+        PlacarMaquina++;
+        
+        alert('O seu placar é '+PlacarJogador+' e o placar do computador '+PlacarMaquina+'.');
+    }
+    
+      else if(MaquinaJogada=="tesoura" && Jogador=="papel")
+    {
+        alert('Você jogou '+Jogador+' e o computador '+MaquinaJogada+'.');
+        alert("Você perdeu!!!");
+        PlacarMaquina++;
+        
+        alert('O seu placar é '+PlacarJogador+' e o placar do computador '+PlacarMaquina+'.');
+    }
+    
+    debugger;
+}
+
+
+    
